8b9362c39ea0ad20f20afac1b6bf5448425a10af
Mariel Godoy Pinpin <marielgodoypinpin@gmail.com>
2026-07-02 19:53:11 +0200

    Creacion de componente
    he creado el componente blog y lo he enlazado con app-root

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Blog } from './components/blog/blog';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Blog],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
